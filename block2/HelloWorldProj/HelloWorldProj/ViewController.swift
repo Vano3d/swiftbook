53c05cb724a347b5a5f72fde13b1f3b5fd0c1e9c
Ivan Ch <vano3d@gmail.com>
2021-05-18 00:07:36 +0300

    save

---
+++ block2/HelloWorldProj/HelloWorldProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldProj
+//
+//  Created by Chupinsky Ivan on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
