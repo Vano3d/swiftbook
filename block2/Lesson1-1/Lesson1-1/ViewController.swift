f0acdecb99b54df05c1803de9d86972260566c5f
Ivan Ch <vano3d@gmail.com>
2021-05-19 00:57:49 +0300

    save

---
+++ block2/Lesson1-1/Lesson1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson1-1
+//
+//  Created by Chupinsky Ivan on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
